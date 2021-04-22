75e1649c7bf71f1e95aa61447fc51b05e40e95a0
Pablo Maribondo <pablomgvieira@gmail.com>
2021-04-22 19:23:37 -0300

    (server) setup do mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
 
-const port = 3333;
-app.listen(port);
+const URI = process.env.MONGO_ATLAS_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const PORT = 3333;
+app.listen(PORT);
