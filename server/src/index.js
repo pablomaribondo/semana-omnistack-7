566d8274c0200fc1fa86882a11aad4f17a587dbd
Pablo Maribondo <pablomgvieira@gmail.com>
2021-04-23 08:55:51 -0300

    (server) cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const routes = require("./routes");
@@ -9,6 +11,8 @@ const app = express();
 
 const URI = process.env.MONGO_ATLAS_URI;
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
 
 app.use(
   "/files",
