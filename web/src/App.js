8bc40c3f8e4f4ccef3877d3c1a0f1338cb5f4836
Pablo Maribondo <pablomgvieira@gmail.com>
2021-04-23 11:12:59 -0300

    (web) header implementado

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
 
+import Header from "./components/Header";
+
 const App = () => (
   <BrowserRouter>
+    <Header />
     <Routes />
   </BrowserRouter>
 );
