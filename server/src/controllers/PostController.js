d26abbe85c057f19c1f9710f42f60176b4eda523
Pablo Maribondo <pablomgvieira@gmail.com>
2021-04-23 08:26:57 -0300

    (server) rota para listagem de posts

--- server/src/controllers/PostController.js
+++ server/src/controllers/PostController.js
@@ -1,7 +1,11 @@
 const Post = require("../models/Post");
 
 module.exports = {
-  async index(request, response) {},
+  async index(_request, response) {
+    const posts = await Post.find().sort("-createdAt");
+
+    return response.json(posts);
+  },
 
   async store(request, response) {
     const { author, place, description, hashtags } = request.body;
